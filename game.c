a8fcd0b56182f74b0ba9a35e74bc4a48dd4624da
Learn C code <114216370+LTycode@users.noreply.github.com>
2022-10-09 14:57:55 +0800

    Create game.c

---
+++ game.c
@@ -0,0 +1,141 @@
+#include"game.h";
+
+void Initboard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+void Displayboard(char board[ROW][COL], int row, int col)
+{
+	//打印行
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j < col - 1)
+			{
+				printf("|");//   |   |   
+			}
+		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j < col - 1)
+				{
+					printf("|");
+				}//---|---|---
+			}
+		}
+		printf("\n");
+		//一次循环
+		//   |   |
+		//---|---|---
+
+	}
+
+}
+void Playermove(char board[ROW][COL], int row, int col)
+{
+	int x, y;
+	printf("玩家下棋\n");
+	printf("请输入坐标>:");
+	while (1)
+	{
+		scanf("%d%d", &x, &y);
+		if (x >= 1 && x <= 3 && y >= 1 && y <= 3)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] ='*';
+				break;
+			}
+			else
+			{
+				printf("该坐标被占用\n");
+			}
+		}
+		else
+		{
+			printf("输入的坐标非法.\n");
+		}
+	}
+}
+void Computermove(char board[ROW][COL], int row, int col)
+{
+	int x, y;
+	printf("电脑走:\n");
+	while (1)
+	{
+		 x = rand() % row;
+		 y = rand() % col;
+		if (board[x ][y ] == ' ')
+		{
+			board[x ][y ] = '#';
+			break;
+		}
+	}
+}
+char Iswin(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	//判断行
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')
+		{
+			return board[i][1];
+		}
+	}
+	//判断列
+	for (i = 0; i < col; i++)
+	{
+		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[1][i] != ' ')
+		{
+			return board[1][i];
+		}
+	}
+	//对角线
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	//是否平局
+	if(1==Isfull(board, ROW, COL))
+	{
+		return 'Q';
+	}
+	return 'C';
+}
+int Isfull(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
